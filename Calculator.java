8160c527ea097cb20937af1d5128d432c042155b
Shubham Shendurkar <shubham.shendurkar@gmail.com>
2021-09-20 23:22:07 +0530

    2 new files added

---
+++ Calculator.java
@@ -0,0 +1,38 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] arg)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b= sc.nextInt();
+        int c = sc.nextInt();
+        if(c==1)
+        {
+            System.out.println("Addition"+(a+b));
+        }
+
+        else if(c==2)
+        {
+            System.out.println("Subtraction"+ (a-b));
+
+        }
+        else if(c==3)
+        {
+            System.out.println("Multiplication"+(a*b));
+        }
+        else if(c==4)
+        {
+            System.out.println("Division"+(a/b));
+        }
+        else if (c==5)
+        {
+            System.out.println("Modulo"+(a%b));
+        }
+        else
+            System.out.println("number are invalid");
+
+
+    }
+}
